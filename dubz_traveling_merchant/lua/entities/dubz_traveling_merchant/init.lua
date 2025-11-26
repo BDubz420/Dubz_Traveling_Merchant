260c6b470a4009a405a122d825c6513b731ccc23
BDubz420 <129692319+BDubz420@users.noreply.github.com>
2025-11-26 01:15:25 -0500

    Add traveling merchant spawn tools and buying system

--- dubz_traveling_merchant/lua/entities/dubz_traveling_merchant/init.lua
+++ dubz_traveling_merchant/lua/entities/dubz_traveling_merchant/init.lua
@@ -2,136 +2,149 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
-include("autorun/franks_snow_config.lua")
+include("autorun/dubz_traveling_merchant_config.lua")
 
-util.AddNetworkString("drugdealeropenmenu")
-util.AddNetworkString("sellcoke")
+util.AddNetworkString("dtm_openmerchant")
+util.AddNetworkString("dtm_buyitem")
 
-local PRICE_REFRESH_TIME = 120
-local CFG = FranksSnowPacking.Config
+local CFG = DubzTravelingMerchant.Config
 
----------------------------------------------------------
--- Dirty Money Helper Wrappers (Use your new system)
----------------------------------------------------------
-function AddDirtyMoney(ply, amount)
-    if not IsValid(ply) then return end
-    ply:AddDirtyMoney(amount)      -- from sv_dirtymoney.lua
+local function notify(ply, msg, level)
+    if DarkRP and DarkRP.notify then
+        DarkRP.notify(ply, level or 0, 4, msg)
+    else
+        ply:ChatPrint(msg)
+    end
 end
 
-function TakeDirtyMoney(ply, amount)
-    if not IsValid(ply) then return end
-    ply:TakeDirtyMoney(amount)
+local function chooseSpawn()
+    local spawns = DubzTravelingMerchant.GetSpawnLocations()
+    if not istable(spawns) or #spawns == 0 then return end
+
+    return table.Random(spawns)
 end
 
+local function giveItem(ply, item)
+    if item.type == "weapon" then
+        ply:Give(item.class)
+        return true
+    elseif item.type == "entity" then
+        local ent = ents.Create(item.class)
+        if not IsValid(ent) then return false end
 
----------------------------------------------------------
--- NPC INITIALIZATION
----------------------------------------------------------
+        local aim = ply:GetAimVector()
+        local pos = ply:GetShootPos() + aim * 32
+
+        ent:SetPos(pos)
+        ent:SetAngles(Angle(0, ply:EyeAngles().y, 0))
+        if item.model then
+            ent:SetModel(item.model)
+        end
+
+        ent:Spawn()
+        ent:Activate()
+        return true
+    elseif item.type == "ammo" then
+        local ammoType = item.ammoType or ""
+        local amount = item.amount or 0
+        if ammoType == "" or amount <= 0 then return false end
+
+        ply:GiveAmmo(amount, ammoType, true)
+        return true
+    end
+
+    return false
+end
+
+-- NPC INITIALIZATION ------------------------------------------------------
 function ENT:Initialize()
-    self:SetModel(table.Random(CFG.Buyer.Models))
-
+    self:SetModel(table.Random(CFG.Merchant.Models))
     self:SetHullType(HULL_HUMAN)
     self:SetHullSizeNormal()
     self:SetNPCState(NPC_STATE_SCRIPT)
     self:SetSolid(SOLID_BBOX)
     self:SetUseType(SIMPLE_USE)
-    self:DropToFloor()
     self:SetTrigger(true)
 
-    local sellprice = math.random(CFG.Buyer.MinSellPrice, CFG.Buyer.MaxSellPrice)
+    local spawn = chooseSpawn()
+    if spawn then
+        self:SetPos(spawn.pos)
+        self:SetAngles(spawn.ang or Angle(0, 0, 0))
+    end
 
-    self:SetCokeSellPrice(sellprice)
-    self.NextPriceRefresh = CurTime() + PRICE_REFRESH_TIME
+    self:DropToFloor()
 
-    self:SetDealerName(table.Random(CFG.Buyer.Names))
-    self:SetCokeHolding(0)
+    self:SetMerchantName(table.Random(CFG.Merchant.Names))
 end
 
 function ENT:OnTakeDamage()
     return 0
 end
 
-
----------------------------------------------------------
--- PICK UP SNOW BRICKS / CRATES
----------------------------------------------------------
-function ENT:StartTouch(ent)
-    if not IsValid(ent) then return end
-
-    -- Loose snow brick
-    if ent:GetClass() == "snow_moneybrick" then
-        self:SetCokeHolding(self:GetCokeHolding() + 1)
-        ent:Remove()
-        return
-    end
-
-    -- Crate with stored bricks
-    if ent:GetClass() == "snow_crate" then
-        local stored = ent:GetNWInt("Stored", 0)
-        if stored > 0 then
-            self:SetCokeHolding(self:GetCokeHolding() + stored)
-            ent:Remove()
-        end
-        return
-    end
-end
-
-
----------------------------------------------------------
--- PRICE REFRESH
----------------------------------------------------------
-function ENT:Think()
-    if CurTime() >= (self.NextPriceRefresh or 0) then
-        self:SetCokeSellPrice(math.random(500, 1500))
-        self.NextPriceRefresh = CurTime() + PRICE_REFRESH_TIME
-    end
-end
-
-
----------------------------------------------------------
--- PLAYER PRESSES E → Open Menu
----------------------------------------------------------
+-- PLAYER PRESSES E → Open Menu -------------------------------------------
 function ENT:AcceptInput(_, _, activator)
     if not IsValid(activator) or not activator:IsPlayer() then return end
 
-    net.Start("drugdealeropenmenu")
+    net.Start("dtm_openmerchant")
         net.WriteEntity(self)
-        net.WriteInt(self:GetCokeSellPrice(), 24)
-        net.WriteString(self:GetDealerName())
-        net.WriteInt(self:GetCokeHolding(), 24)
+        net.WriteString(self:GetMerchantName())
+        net.WriteUInt(#CFG.Inventory, 8)
+        for _, item in ipairs(CFG.Inventory) do
+            net.WriteString(item.id)
+            net.WriteString(item.name)
+            net.WriteInt(item.price, 32)
+            net.WriteString(item.type)
+        end
     net.Send(activator)
 end
 
-
----------------------------------------------------------
--- PLAYER SELLS SNOW → GETS DIRTY MONEY
----------------------------------------------------------
-net.Receive("sellcoke", function(_, ply)
+-- PURCHASE HANDLER --------------------------------------------------------
+net.Receive("dtm_buyitem", function(_, ply)
     local dealer = net.ReadEntity()
-    if not IsValid(dealer) then return end
+    local itemId = net.ReadString()
 
-    local bricks = dealer:GetCokeHolding()
-    if bricks < 1 then
-        DarkRP.notify(ply, 1, 4, "You have no snow bricks to sell.")
+    if not IsValid(ply) or not IsValid(dealer) or dealer:GetClass() ~= "dubz_traveling_merchant" then return end
+
+    local item
+    for _, data in ipairs(CFG.Inventory) do
+        if data.id == itemId then
+            item = data
+            break
+        end
+    end
+
+    if not item then return end
+
+    if not (ply.canAfford and ply.addMoney) then
+        notify(ply, "DarkRP money functions not available on this server.", 1)
         return
     end
 
-    local sellPrice = dealer:GetCokeSellPrice()
-    local payout = bricks * sellPrice
+    if not ply:canAfford(item.price) then
+        notify(ply, "You cannot afford this item.", 1)
+        return
+    end
 
-    -----------------------------------------------------
-    -- PAY DIRTY MONEY (persistent + networked)
-    -----------------------------------------------------
-    ply:AddDirtyMoney(payout)
+    ply:addMoney(-item.price)
 
-    DarkRP.notify(
-        ply,
-        2,
-        5,
-        "You sold " .. bricks .. " snow brick(s) for " ..
-        DarkRP.formatMoney(payout) .. " (Dirty Money)"
-    )
+    if not giveItem(ply, item) then
+        notify(ply, "Something went wrong while delivering that item.", 1)
+        return
+    end
 
-    -- reset NPC's inventory
-    dealer:SetCokeHolding(0)
+    notify(ply, "Purchased " .. item.name .. " for " .. DarkRP.formatMoney(item.price) .. ".", 0)
+end)
+
+-- RESPAWN SUPPORT ---------------------------------------------------------
+hook.Add("PostCleanupMap", "DTM_AutoRespawn", function()
+    DubzTravelingMerchant.ReloadSpawns()
+
+    if not CFG.Merchant.RespawnDelay or CFG.Merchant.RespawnDelay <= 0 then return end
+
+    timer.Simple(CFG.Merchant.RespawnDelay, function()
+        if not DubzTravelingMerchant.GetSpawnLocations() then return end
+        local ent = ents.Create("dubz_traveling_merchant")
+        if not IsValid(ent) then return end
+        ent:Spawn()
+    end)
 end)
--- dubz_traveling_merchant/lua/entities/dubz_traveling_merchant/shared.lua
+++ dubz_traveling_merchant/lua/entities/dubz_traveling_merchant/shared.lua
@@ -4,21 +4,18 @@ AddCSLuaFile("shared.lua")
 
 ENT.Type = "ai"
 ENT.Base = "base_ai"
-ENT.PrintName = "Snow Buyer"
+ENT.PrintName = "Traveling Merchant"
 ENT.Author = "BDubz420"
-ENT.Category = "Franks Snow Packing"
+ENT.Category = "Dubz Traveling Merchant"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
 ENT.RenderGroup = RENDERGROUP_BOTH
 ENT.AutomaticFrameAdvance = true
 
 function ENT:SetupDataTables()
-    self:NetworkVar("Int", 0, "CokeSellPrice")
-    self:NetworkVar("Int", 1, "CokeHolding")
-    self:NetworkVar("Int", 2, "WantedChance")
-    self:NetworkVar("String", 0, "DealerName")
+    self:NetworkVar("String", 0, "MerchantName")
 end
 
 function ENT:SetAutomaticFrameAdvance(bUsingAnim)
-	self.AutomaticFrameAdvance = bUsingAnim
+    self.AutomaticFrameAdvance = bUsingAnim
 end
